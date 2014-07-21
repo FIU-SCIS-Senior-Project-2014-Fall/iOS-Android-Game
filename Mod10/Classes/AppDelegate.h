61d0f030051426eed555564a3b1776e17b51c0bd
Lorenzo Castillo <lorenzocastillo@lorenzoillosmbp.home>
2014-07-21 07:31:27 -0700

    Initial Commit

---
+++ Mod10/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Mod10
+//
+//  Created by Lorenzo Castillo on 7/21/14.
+//  Copyright Lorenzo Castillo 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
